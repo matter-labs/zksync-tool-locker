ec87782895ebd8ded2ad8684fec34c2ac9716b15
Sergey Beresnev <s.beresnev@matterlabs.dev>
2021-12-11 04:19:23 +0200

    feat: restyled & reconfigured tool

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,265 @@
 <template>
-  <h1>Method to disable CPK on ropsten</h1>
-  <button type="button" id="modal-trigger">Open modal</button>
-  <div>{{msg}}</div>
+  <div class="flex-shrink-0 flex justify-center">
+    <a href="/" class="inline-flex">
+      <span class="sr-only">zkSync Ropsten</span>
+      <a href="https://zksync.io" target="_blank">
+        <svg fill="none" class="h-8 w-auto" viewBox="0 0 1407 276" xmlns="http://www.w3.org/2000/svg">
+          <path clip-rule="evenodd" d="M485.481 137.772L347.819.254944V100.967L211.134 201.801L347.819 201.896V275.289L485.481 137.772Z" fill="#4E529A" fill-rule="evenodd"></path>
+          <path clip-rule="evenodd" d="M0.200116 137.722L137.862 275.239L137.862 175.337L274.547 73.6923L137.862 73.5981L137.862.205097L0.200116 137.722Z" fill="#8C8DFC"
+                fill-rule="evenodd"></path>
+          <path d="M697.395 218.752H591.773V188.421L652.239 121.018H593.33V91.2059H696.357V119.982L634.073 188.68H697.395V218.752Z" fill-opacity="0.9" class="logoLetter"></path>
+          <path d="M841.54 91.2059L790.416 144.869L842.578 218.752H800.278L766.541 170.015L752.268 185.051V218.752H717.752V31.0623H752.268V138.647L796.385 91.2059H841.54Z"
+                fill-opacity="0.9" class="logoLetter"></path>
+          <path
+            d="M991.66 78.7624L959.48 88.6135C958.269 82.0461 954.982 76.0836 949.619 70.726C944.256 65.3684 936.384 62.6895 926.003 62.6895C917.526 62.6895 910.605 65.0227 905.242 69.689C899.879 74.1825 897.197 79.6265 897.197 86.0211C897.197 97.2548 903.858 104.168 917.18 106.76L942.871 111.686C959.134 114.797 971.764 121.278 980.76 131.129C989.757 140.98 994.255 152.559 994.255 165.867C994.255 181.076 988.2 194.383 976.089 205.79C964.152 217.024 948.062 222.64 927.82 222.64C916.228 222.64 905.761 220.912 896.418 217.456C887.076 213.999 879.55 209.419 873.841 203.716C868.132 197.84 863.633 191.704 860.346 185.31C857.232 178.742 855.329 172.002 854.637 165.089L887.855 156.275C888.72 166.126 892.526 174.335 899.273 180.903C906.194 187.47 915.796 190.754 928.079 190.754C937.422 190.754 944.688 188.68 949.878 184.532C955.242 180.384 957.923 174.94 957.923 168.2C957.923 162.842 955.934 158.263 951.954 154.46C948.148 150.485 942.698 147.807 935.605 146.424L909.913 141.239C895.034 138.128 883.27 131.993 874.619 122.833C865.969 113.673 861.644 102.267 861.644 88.6135C861.644 72.3678 867.872 58.7146 880.329 47.6537C892.958 36.5928 908.097 31.0623 925.744 31.0623C936.297 31.0623 945.726 32.5313 954.031 35.4694C962.335 38.4074 968.996 42.3824 974.013 47.3944C979.03 52.2336 982.923 57.2455 985.691 62.4303C988.459 67.6151 990.449 73.0591 991.66 78.7624Z"
+            fill-opacity="0.9" class="logoLetter"></path>
+          <path d="M1052.4 269.044H1015.81L1045.91 202.679L991.674 91.2059H1030.34L1064.34 165.867L1095.74 91.2059H1132.59L1052.4 269.044Z" fill-opacity="0.9"
+                class="logoLetter"></path>
+          <path
+            d="M1180.23 145.387V218.752H1145.71V91.2059H1179.19V107.02C1182.82 100.798 1188.01 96.0451 1194.76 92.7613C1201.51 89.4776 1208.6 87.8358 1216.04 87.8358C1231.09 87.8358 1242.51 92.5885 1250.3 102.094C1258.26 111.427 1262.24 123.524 1262.24 138.388V218.752H1227.72V144.35C1227.72 136.746 1225.73 130.61 1221.75 125.944C1217.94 121.278 1212.06 118.945 1204.1 118.945C1196.84 118.945 1191.04 121.451 1186.72 126.463C1182.39 131.474 1180.23 137.783 1180.23 145.387Z"
+            fill-opacity="0.9" class="logoLetter"></path>
+          <path
+            d="M1346.83 119.204C1337.66 119.204 1329.96 122.401 1323.74 128.796C1317.51 135.19 1314.39 143.918 1314.39 154.979C1314.39 166.04 1317.51 174.854 1323.74 181.421C1330.14 187.816 1337.92 191.013 1347.09 191.013C1355.22 191.013 1361.71 188.939 1366.56 184.791C1371.4 180.471 1374.69 175.372 1376.42 169.496L1406.78 179.607C1403.67 191.359 1396.92 201.469 1386.54 209.938C1376.16 218.406 1363.01 222.64 1347.09 222.64C1328.06 222.64 1312.06 216.246 1299.08 203.457C1286.28 190.495 1279.88 174.335 1279.88 154.979C1279.88 135.449 1286.19 119.29 1298.82 106.501C1311.63 93.7119 1327.37 87.3173 1346.05 87.3173C1362.32 87.3173 1375.64 91.5516 1386.02 100.02C1396.4 108.316 1403.06 118.426 1406 130.351L1375.12 140.721C1370.97 126.376 1361.54 119.204 1346.83 119.204Z"
+            fill-opacity="0.9" class="logoLetter"></path>
+        </svg>
+      </a>
+    </a>
+  </div>
+  <div class="py-8">
+    <div class="text-center">
+      <p class="text-sm font-semibold text-indigo-600 uppercase tracking-wide">ropsten</p>
+      <h1 class="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Unset CPK</h1>
+      <p class="mt-2 text-base text-gray-500">This tool allows you to unset zkSync's CPK on testnets </p>
+      <div class="mt-6">
+        <button
+          @click="connect"
+          type="button"
+          class="overflow-visible justify-center items-center py-2 px-5 my-8 mx-auto text-xl font-normal leading-normal text-center text-white normal-case align-middle bg-indigo-800 rounded border border-indigo-800 border-solid cursor-pointer select-none box-border whitespace-no-wrap"
+          style="transition: background-color 0.21s ease 0s, border-color 0.21s ease 0s, color 0.21s ease 0s;"
+        >
+          Connect your wallet
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="w-2/3 flow-root mx-auto" v-if="logs.length > 0">
+    <p class="text-sm font-semibold text-indigo-600 mb-4 uppercase tracking-wide">Log board</p>
+    <ul role="list" class="-mb-8">
+      <li v-for="oneLog in logs" :key="oneLog.time" :class="{'bg-red-400': oneLog.severity === 'error','bg-yellow-200': oneLog.severity === 'warning'}">
+        <div class="relative pb-8">
+          <span class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="px-3 py-1 rounded font-bold text-xs bg-gray-400 flex items-center justify-center ring-8 ring-white">{{ oneLog.severity }}</span>
+            </div>
+            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-2 overflow-hidden">
+              <div>
+                <p class="text-sm text-gray-500">{{ oneLog.text }}</p>
+              </div>
+              <div class="text-right text-sm whitespace-nowrap text-gray-500">
+                <time>{{ oneLog.time.toLocaleTimeString() }}</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
-import { ethers } from "ethers";
+import { providers } from "ethers";
 import { getDefaultProvider, Wallet as zkWallet, Signer as zkSigner } from "zksync";
 
-export default defineComponent({
-  name: "App",
-  props: {
-    msg: String
-  }
-})
-
-const connect = async () => {
-
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        infuraId: "72b13ae588b44eb9bc04c571bf22c984"
-      }
-    }
-  };
-
-  const web3Modal = new Web3Modal({
-    network: "ropsten", // optional
-    cacheProvider: false, // optional
-    providerOptions // required
-  });
-
-  try {
-
-    web3Modal.clearCachedProvider();
-
-    const provider = await web3Modal.connect();
-
-    console.log(provider);
-
-
-    const ethersProvider = new ethers.providers.Web3Provider(provider);
-    const ethSigner = await ethersProvider.getSigner();
-    const signMessage = await ethSigner.signMessage("hello-world");
-    console.warn("sign: ", signMessage);
-    const zksyncProvider = await getDefaultProvider("ropsten", "HTTP");
-    const wallet = await zkWallet.fromEthSignerNoKeys(ethSigner, zksyncProvider);
-    wallet.ethSignerType = {
-      verificationMethod: "ERC-1271",
-      // Indicates if signer adds `\x19Ethereum Signed Message\n${msg.length}` prefix before signing message.
-      // i.e. if false, we should add this prefix manually before asking to sign message
-      isSignedMsgPrefixed: true
+export default {
+  data: function () {
+    return {
+      logs: [] as unknown[],
+      isLoading: false as boolean,
+      provider: null as providers.Provider | null
     };
-    wallet.signer = await zkSigner.fromSeed(new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3,
-      4, 5, 6, 7, 8, 9]));
-    const keySet = await wallet.isSigningKeySet();
-    console.warn("is key set?:", keySet);
-    const tx = await wallet.onchainAuthSigningKey();
-    console.warn("tx: ", tx.hash);
-    await tx.wait();
+  },
+  mounted () {
+    this.recordLog("Loaded!");
+  },
+  methods: {
+    recordLog (message: string, severity: "log" | "warning" | "error" = "log") {
+      this.logs.push({
+        text: message,
+        severity,
+        time: new Date()
+      });
+    },
+    async connect () {
+      this.beforeConnect();
+      try {
 
-    const zkTx = await wallet.setSigningKey({ feeToken: "ETH", ethAuthType: "Onchain" });
-    console.warn("zk txn link", `https://rinkeby.zkscan.io/explorer/transactions/${zkTx.txHash}`);
-  } catch (error) {
-    console.error(error);
-    alert(error.message);
+        this.recordLog("started new connection");
+        const providerOptions = {
+          walletconnect: {
+            package: WalletConnectProvider, // required
+            options: {
+              infuraId: "b818c25e42c049faa539560425a18a4e" // required
+            }
+          }
+        };
+
+        this.recordLog("trigger popup w/t infura key & ropsten network");
+
+        const web3Modal = new Web3Modal({
+          network: "ropsten", // optional
+          providerOptions // required
+        });
+
+        this.recordLog("clearing cached provider (if any)");
+
+        const provider = await web3Modal.connect();
+
+        console.log(provider);
+
+        this.recordLog("provider received");
+
+        this.provider = new providers.Web3Provider(provider);
+
+        this.recordLog("ethers web3 provider received");
+
+        const ethSigner = await this.provider.getSigner();
+
+        this.recordLog("signer received");
+
+        this.recordLog("trying to sign “hello-world” for test");
+        const signMessage = await ethSigner.signMessage("hello-world");
+        this.recordLog("signing results: " + signMessage);
+
+        const zksyncProvider = await getDefaultProvider("ropsten", "HTTP");
+
+        this.recordLog("zksyncProvider received");
+        const wallet = await zkWallet.fromEthSignerNoKeys(ethSigner, zksyncProvider);
+
+        this.recordLog("zkWallet received, getting signer");
+
+        wallet.ethSignerType = {
+          verificationMethod: "ERC-1271",
+          // Indicates if signer adds `\x19Ethereum Signed Message\n${msg.length}` prefix before signing message.
+          // i.e. if false, we should add this prefix manually before asking to sign message
+          isSignedMsgPrefixed: true
+        };
+
+        this.recordLog("awaiting signer from seed...");
+        wallet.signer = await zkSigner.fromSeed(new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3,
+          4, 5, 6, 7, 8, 9]));
+
+        this.recordLog("checking isSigningKeySet...");
+
+        const keySetBefore = await wallet.isSigningKeySet();
+
+        this.recordLog("isSigningKeySet status for `ropsten` (wallet address: " + wallet.address() + ") : " + (keySetBefore ? "signed" : "unsigned"), keySetBefore ? "log" :
+          "warning");
+
+        if (!keySetBefore) {
+          throw new Error("key not set");
+        }
+
+        this.recordLog("signer from seed received");
+
+        const tx = await wallet.onchainAuthSigningKey();
+
+        this.recordLog("onchainAuthSigningKey tx hash: " + tx.hash + ")");
+
+        this.recordLog("awaiting 10 confirmations");
+
+        await tx.wait(10);
+
+        this.recordLog("Confirmed! Calling setSigning key with feeToken ETH & ethAuthType Onchain...");
+
+        const zkTx = await wallet.setSigningKey({ feeToken: "ETH", ethAuthType: "Onchain" });
+
+        this.recordLog("Operation formed (url: " + `https://ropsten.zkscan.io/explorer/transactions/${zkTx.txHash}`);
+
+      } catch (err) {
+        console.error(err)
+        this.recordLog(err?.message || err as string, "error");
+        this.provider = null;
+      }
+    },
+    beforeConnect (): void {
+      this.recordLog("Before the connection clearing all from previous wallet");
+      if (this.provider) {
+        console.log(this.provider);
+        this.recordLog("found active provider", "warning");
+        // await this.provider!.ws!.disconnect();
+        this.provider = null;
+      }
+      this.recordLog(window.localStorage.getItem("walletconnect") as string);
+      localStorage.removeItem("walletconnect");
+    }
   }
 };
-
-document.addEventListener('readystatechange', () => {
-  document.querySelector('#modal-trigger')?.addEventListener('click', connect)
-})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="css">
+.logoLetter {
+  fill: #11142b;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1, h2 {
+  color: rgb(36, 57, 85);
+  display: block;
+  font: 700 30px/45px "Fira Sans", sans-serif;
+  height: 45px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h2 {
+  height: 30px;
+  font-size: 20px;
+  line-height: 30px;
+  margin: 20px auto;
+  font-weight: 500;
+}
+
+button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 7px 12px 7px 7px;
+  width: fit-content;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .1);
+  appearance: none;
+  border-radius: 4px;
+  border-image-repeat: stretch;
+  border-image-source: none;
+  border: 0 none rgb(255, 255, 255);
+  box-sizing: border-box;
+  color: rgb(255, 255, 255);
+  font: 400 16px/22px "Fira Sans", sans-serif;
+  margin: 20px 10px;
+  min-width: 200px;
+  outline: rgb(255, 255, 255) none 0;
+  text-align: center;
+  text-indent: 0;
+  text-rendering: auto;
+  text-shadow: none;
+  text-transform: none;
+  transition: opacity .2s ease;
+  white-space: nowrap;
+  will-change: opacity;
+  word-spacing: 0;
+  writing-mode: horizontal-tb;
+  cursor: pointer;
+  opacity: .7;
+}
+
+button:hover {
+  opacity: 1;
 }
 </style>
